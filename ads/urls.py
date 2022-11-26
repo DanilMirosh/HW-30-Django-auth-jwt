ae4ed72be74d155a57f11f6c79ca550f3a879677
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-11-26 22:28:24 +0500

    add url selection

--- ads/urls.py
+++ ads/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 
 from ads.views import *
 from ads.views.location import LocationViewSet
+from ads.views.selection import *
 
 router = routers.SimpleRouter()
 router.register('location', LocationViewSet)
@@ -19,7 +20,13 @@ urlpatterns = [
     path('cat/<int:pk>/', CategoryDetailView.as_view()),
     path('cat/<int:pk>/update/', CategoryUpdateView.as_view()),
     path('cat/<int:pk>/delete/', CategoryDeleteView.as_view()),
-    path('cat/create/', CategoryCreateView.as_view())
+    path('cat/create/', CategoryCreateView.as_view()),
+    path('selection/', SelectionListView.as_view()),
+    path('selection/<int: pk>/', SelectionDetailView.as_view()),
+    path('selection/<int: pk>/update/', SelectionUpdateView.as_view()),
+    path('selection/<int: pk>/delete/', SelectionDeleteView.as_view()),
+    path('selection/create/', SelectionCreateView.as_view()),
+
 ]
 
 urlpatterns += router.urls
